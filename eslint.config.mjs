8184393ed41f973f7d38be0d0cce572232d563ad
jordanNDE <ndetcho.jordan@gmail.com>
2025-06-14 13:59:38 +0200

    remove linting errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,28 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = {
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  rules: {
+    ...eslint.configs.recommended.rules,
+    ...tseslint.configs.recommended.rules,
+    "prefer-const": "warn",
+    "no-constant-binary-expression": "error",
+
+    // Disable no-undef as TypeScript handles this better
+    "no-undef": "off",
+    "@typescript-eslint/no-unsafe-function-type": "off",
+    // Customize TypeScript rules
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            ignoreRestSiblings: true,
+        },
+    ],
+},
+};
 
 export default eslintConfig;
